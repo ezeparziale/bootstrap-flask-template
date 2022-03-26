87598af677ccd4a07d1972d902ca5a415f77a9e3
Ezequiel Parziale <eparziale@gmail.com>
2022-03-26 01:18:15 -0300

    :star: feature: modulo nuevo about

---
+++ app/about/about.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template
+
+about_bp = Blueprint("about", __name__, template_folder='templates')
+
+@about_bp.route("/about/")
+def about_view():
+    return render_template("about.html")
