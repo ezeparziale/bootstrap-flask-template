1b896e07f9177b53a627a51711e2d35168d3cfbb
Ezequiel Parziale <eparziale@gmail.com>
2022-03-26 01:19:04 -0300

    :rocket: update: register blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 
-from .home import view
-app.register_blueprint(view.home_bp)
+from .home import home
+app.register_blueprint(home.home_bp)
+
+from .about import about
+app.register_blueprint(about.about_bp)
