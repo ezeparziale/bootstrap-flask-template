6a77c04ef4fc3b687b67c1c1a682e51249be7a52
Ezequiel Parziale <eparziale@gmail.com>
2022-03-26 22:06:17 -0300

    :rocket: add: flash messages

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, render_template, url_for
+from flask import Blueprint, redirect, render_template, url_for, flash
 from .forms import RegistrationForm, LoginForm
 
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth", template_folder='templates')
@@ -7,7 +7,11 @@ auth_bp = Blueprint("auth", __name__, url_prefix="/auth", template_folder='templ
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect(url_for("home.home_view"))
+        if form.email.data=="test@test.com" and form.password.data=="123456":
+            flash(f"Login OK", category="success")
+            return redirect(url_for("home.home_view"))
+        else:
+            flash(f"Error al loguearse", category="danger")
     return render_template("login.html", form=form)
 
 
@@ -15,5 +19,6 @@ def login():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
+        flash(f"Cuenta creada exitosamente", category="success")
         return redirect(url_for("auth.login"))
     return render_template("register.html", form=form)
