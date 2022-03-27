8ee080a05f3146a78f7190e56395c9ee6bcc7c05
Ezequiel Parziale <eparziale@gmail.com>
2022-03-27 00:36:46 -0300

    :star: feature: postgres db

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "SECRET_KEY"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database/app.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://flaskuser:flaskpassword@localhost:5432/flaskdb"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 db.create_all()
