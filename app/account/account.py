8ce18b7195013767f6a8e5d37834029ec46ba539
Ezequiel Parziale <eparziale@gmail.com>
2022-03-27 11:21:39 -0300

    :rocket: update: account solo con login

--- app/account/account.py
+++ app/account/account.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, redirect, render_template
+from flask_login import login_required
 
 account_bp = Blueprint("account", __name__, url_prefix="/account", template_folder='templates')
 
 @account_bp.route("/", methods=["GET", "POST"])
+@login_required
 def account():
     return render_template("account.html")
