9b857b739a10ea6dd234117547733c1723e62c00
Ezequiel Parziale <eparziale@gmail.com>
2022-03-26 23:47:21 -0300

    :rocket: add: database

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "SECRET_KEY"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database/app.db"
+
+db = SQLAlchemy(app)
+db.create_all()
 
 from .home import home
 app.register_blueprint(home.home_bp)
