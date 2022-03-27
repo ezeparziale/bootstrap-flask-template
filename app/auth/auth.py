fab432051916a0693d2bc801008d9e794c12a2d3
Ezequiel Parziale <eparziale@gmail.com>
2022-03-26 21:51:28 -0300

    :rocket: update: login and register view
    - Se utilizan los forms

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -1,12 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, url_for
+from .forms import RegistrationForm, LoginForm
 
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth", template_folder='templates')
 
-@auth_bp.route("/login/")
+@auth_bp.route("/login/", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect(url_for("home.home_view"))
+    return render_template("login.html", form=form)
 
 
-@auth_bp.route("/register/")
+@auth_bp.route("/register/", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        return redirect(url_for("auth.login"))
+    return render_template("register.html", form=form)
