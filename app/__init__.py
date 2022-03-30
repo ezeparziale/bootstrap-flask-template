f65815e3285724d763ef415d843381984c59d400
Ezequiel Parziale <eparziale@gmail.com>
2022-03-30 18:56:18 -0300

    :bulb: add: comments init

--- app/__init__.py
+++ app/__init__.py
@@ -6,15 +6,18 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from .config import settings
 
+# Flask
 app = Flask(__name__)
 app.config.from_object(settings)
 
-
+# Database
 db = SQLAlchemy(app)
 db.create_all()
 
+# Bcrypt
 bcrypt = Bcrypt(app)
 
+# Login Manager
 login_manager = LoginManager(app)
 login_manager.refresh_view = "auth.login"
 login_manager.needs_refresh_message = (
@@ -22,8 +25,10 @@ login_manager.needs_refresh_message = (
 )
 login_manager.needs_refresh_message_category = "info"
 
+# Mail
 mail = Mail(app)
 
+# Blueprints
 from .home import home
 app.register_blueprint(home.home_bp)
 
