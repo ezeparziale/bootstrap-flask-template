807c68260e065dc62a207a33b5992d4b5305d1f3
Ezequiel Parziale <eparziale@gmail.com>
2022-04-05 21:37:14 -0300

    :speech_balloon: update: upper labels

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -5,17 +5,17 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 class RegistrationForm(FlaskForm):
-    username = StringField(label="Username", validators=[DataRequired(), Length(min=3, max=20)])
-    email = StringField(label="Email", validators=[DataRequired(), Email()])
-    password = PasswordField(label="Password", validators=[DataRequired(), Length(min=6, max=16)])
-    confirm_password = PasswordField(label="Confirm Password", validators=[DataRequired(), EqualTo("password")])
-    submit = SubmitField(label="Sign Up")
+    username = StringField(label="USUARIO", validators=[DataRequired(), Length(min=3, max=20)])
+    email = StringField(label="EMAIL", validators=[DataRequired(), Email()])
+    password = PasswordField(label="PASSWORD", validators=[DataRequired(), Length(min=6, max=16)])
+    confirm_password = PasswordField(label="CONFIRMAR PASSWORD", validators=[DataRequired(), EqualTo("password")])
+    submit = SubmitField(label="Registrar")
 
 class LoginForm(FlaskForm):
-    email = StringField(label="Email", validators=[DataRequired(), Email()])
-    password = PasswordField(label="Password", validators=[DataRequired(), Length(min=6, max=16)])
-    remember_me = BooleanField(label="Remember me")
-    submit = SubmitField(label="Login")
+    email = StringField(label="EMAIL", validators=[DataRequired(), Email()])
+    password = PasswordField(label="PASSWORD", validators=[DataRequired(), Length(min=6, max=16)])
+    remember_me = BooleanField(label="Recuérdame")
+    submit = SubmitField(label="Iniciar sesión")
 
 class ResetPasswordRequestForm(FlaskForm):
     email = StringField(label="EMAIL", validators=[DataRequired(), Email()])
