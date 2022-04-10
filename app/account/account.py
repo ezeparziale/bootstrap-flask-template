c806fe038e978bdd2280307ec842e9eef5ed7d39
Ezequiel Parziale <eparziale@gmail.com>
2022-04-10 20:06:49 -0300

    :rocket: update: new account endpoints

--- app/account/account.py
+++ app/account/account.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, redirect, render_template, request, url_for, flash
 from flask_login import current_user, fresh_login_required, login_required
-from .forms import AccountUpdateForm
+from .forms import AccountInfoForm, AccountUpdateForm
 from app import db, app
 import os
 from app.auth.models import UserDetails
@@ -13,10 +13,27 @@ def save_image(picture_file):
     picture_file.save(picture_path)
     return picture_name
 
+
 @account_bp.route("/", methods=["GET", "POST"])
 @login_required
-@fresh_login_required
+# @fresh_login_required
 def account():
+    form = AccountInfoForm()
+    if form.validate_on_submit():
+        return redirect(url_for("account.edit_account"))
+    form.email.data = current_user.email
+    form.username.data = current_user.username
+    if current_user.details:
+            form.firstname.data = current_user.details.firstname
+            form.lastname.data = current_user.details.lastname
+    image_url = url_for("static", filename="img/profiles/" + current_user.image_file)
+    return render_template("account.html", form=form, image_url=image_url)
+    
+
+@account_bp.route("/edit", methods=["GET", "POST"])
+@login_required
+# @fresh_login_required
+def edit_account():
     form = AccountUpdateForm()
     if form.validate_on_submit():
         if form.picture.data:
@@ -47,4 +64,4 @@ def account():
             form.firstname.data = current_user.details.firstname
             form.lastname.data = current_user.details.lastname
     image_url = url_for("static", filename="img/profiles/" + current_user.image_file)
-    return render_template("account.html", form=form, image_url=image_url)
+    return render_template("edit_account.html", form=form, image_url=image_url)
