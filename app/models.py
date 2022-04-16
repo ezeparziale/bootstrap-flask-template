c81fa27d34f17e5c809d63d70f953699f17ed437
Ezequiel Parziale <eparziale@gmail.com>
2022-04-15 21:42:14 -0300

    :star: feature: add roles and follow view

--- app/models.py
+++ app/models.py
@@ -1,9 +1,9 @@
 from flask import current_app, redirect, url_for
-from app import db, login_manager
+from app import app, db, login_manager
 from sqlalchemy import Column, Integer, String, BOOLEAN, ForeignKey, Text
 from sqlalchemy.sql.sqltypes import DateTime, TIMESTAMP
 from sqlalchemy.sql.expression import text
-from flask_login import UserMixin
+from flask_login import UserMixin, AnonymousUserMixin
 import jwt
 from datetime import datetime, timedelta, timezone
 
@@ -51,6 +51,12 @@ class User(db.Model, UserMixin):
         cascade="all, delete-orphan"
     )
     comments = db.relationship("Comment", backref="author", lazy="dynamic")
+    rol_id = Column(Integer, ForeignKey("roles.id", ondelete="CASCADE"), nullable=False)
+
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.role is None:
+            self.role = Role.query.filter_by(default=True).first()
 
     def __repr__(self) -> str:
         return f"{self.username} : {self.email} : {self.created_at}"
@@ -117,6 +123,22 @@ class User(db.Model, UserMixin):
     def followed_posts(self):
         return Post.query.join(Follow, Follow.followed_id == Post.author_id).filter(Follow.follower_id == self.id)
 
+    def can(self, perm):
+        return self.role is not None and self.role.has_permission(perm)
+
+    def is_admin(self):
+        return self.can(Permission.ADMIN)
+
+
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, perm):
+        return False
+
+    def is_admin(self):
+        return False
+
+login_manager.anonymous_user = AnonymousUser
+
 class UserDetail(db.Model):
     __tablename__ = "user_details"
     id = Column(Integer, primary_key=True)
@@ -144,3 +166,63 @@ class Comment(db.Model):
     created_at = Column(TIMESTAMP(timezone=True), server_default=text("CURRENT_TIMESTAMP"), nullable=False)
     post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), nullable=False)
     author_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
+class Role(db.Model):
+    __tablename__ = "roles"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(40), unique=True, nullable=False)
+    default = Column(BOOLEAN, default=False, index=True)
+    permissions = Column(Integer, nullable=False)
+    users = db.relationship("User", backref="role", lazy="dynamic")
+
+    def __init__(self, **kwargs):
+        super(Role, self).__init__(**kwargs)
+        if self.permissions is None:
+            self.permissions = 0
+
+    def add_permission(self, perm):
+        if not self.has_permission(perm):
+            self.permissions += perm
+    
+    def remove_permission(self, perm):
+        if self.has_permission(perm):
+            self.permissions -= perm
+    
+    def reset_permissions(self):
+        self.permissions = 0
+
+    def has_permission(self, perm):
+        return self.permissions & perm == perm
+
+    def __repr__(self) -> str:
+        return f"{self.name}"
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            "User": [Permission.FOLLOW, Permission.COMMENT, Permission.WRITE_POSTS],
+            "Moderator": [Permission.FOLLOW, Permission.COMMENT, Permission.WRITE_POSTS, Permission.MODERATE],
+            "Admin": [Permission.FOLLOW, Permission.COMMENT, Permission.WRITE_POSTS, Permission.MODERATE, Permission.ADMIN],
+        }
+        default_role = "User"
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.reset_permissions()
+            for perm in roles[r]:
+                role.add_permission(perm)
+            role.default = (role.name == default_role)
+            db.session.add(role)
+        db.session.commit()
+
+class Permission:
+    FOLLOW = 0x01
+    COMMENT = 0x02
+    WRITE_POSTS = 0x04
+    MODERATE = 0x08
+    ADMIN = 0x80
+
+@app.context_processor
+def inject_permission():
+    return dict(Permission=Permission)
--- app/user/user.py
+++ app/user/user.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, redirect, render_template, request, url_for, flash
-from flask_login import current_user, fresh_login_required, login_required
-from app import db, app
-import os
+from flask_login import current_user, login_required
+from app.config import settings
 from ..models import User
 
 user_bp = Blueprint("user", __name__, url_prefix="/user", template_folder='templates')
@@ -20,6 +19,9 @@ def follow(username: str):
     if user == current_user:
         flash("No te puede auto seguir", category="info")
         return redirect(url_for("user.user", username=username))
+    if current_user.is_following(user):
+        flash("Ya estás siguiendo a este usuario", category="info")
+        return redirect(url_for("user.user", username=username))
     current_user.follow(user)
     flash(f"Estas siguiendo a {username}", category="success")
     return redirect(url_for("user.user", username=username))
@@ -32,6 +34,34 @@ def unfollow(username: str):
     if user == current_user:
         flash("No te puedes auto seguir", category="info")
         return redirect(url_for("user.user", username=username))
+    if not current_user.is_following(user):
+        flash("No estás siguiendo a este usuario", category="info")
+        return redirect(url_for("user.user", username=username))
     current_user.unfollow(user)
     flash(f"Ya no sigues a {username}", category="success")
     return redirect(url_for("user.user", username=username))
+
+
+@user_bp.route("/followers/<username>", methods=["GET"])
+@login_required
+def followers(username: str):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash("Usuario invalido", category="info")
+        return redirect(url_for("posts.posts", username=username))
+    page = request.args.get("page", 1, type=int)
+    pagination = user.followers.paginate(page, settings.POSTS_PER_PAGE, False)
+    followers = pagination.items
+    return render_template("followers.html", followers=followers, pagination=pagination, user=user)
+
+@user_bp.route("/follow_by/<username>", methods=["GET"])
+@login_required
+def follow_by(username: str):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash("Usuario invalido", category="info")
+        return redirect(url_for("posts.posts", username=username))
+    page = request.args.get("page", 1, type=int)
+    pagination = user.followed.paginate(page, settings.POSTS_PER_PAGE, False)
+    followed = pagination.items
+    return render_template("followed_by.html", followed=followed, pagination=pagination, user=user)
