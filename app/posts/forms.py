eb98450b9554bfc2fa2349ffaecad31d254a92d1
Ezequiel Parziale <eparziale@gmail.com>
2022-05-25 18:33:37 -0300

    :rocket: update: add tag in post form

--- app/posts/forms.py
+++ app/posts/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, TextAreaField, StringField
+from wtforms import SubmitField, TextAreaField, StringField, SelectMultipleField
 from wtforms.validators import DataRequired, Length
+from ..models import Tag
+
 
 class PostViewForm(FlaskForm):
     title = StringField(label="Titulo", validators=[DataRequired(), Length(min=5, max=40)])
@@ -9,6 +11,7 @@ class PostViewForm(FlaskForm):
 class PostForm(FlaskForm):
     title = StringField(label="Titulo", validators=[DataRequired(), Length(min=5, max=40)])
     content = TextAreaField(label="Contenido", validators=[DataRequired(), Length(min=10)])
+    tags = SelectMultipleField(label="Tags", validators=[DataRequired()], choices=[(tag.id, tag.name) for tag in Tag.query.all()], coerce=int)
     submit = SubmitField(label="Postear")
 
 class PostCommentForm(FlaskForm):
