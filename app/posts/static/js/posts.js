6047bc2419125bef1049afa564f8e49ce19986fe
Ezequiel Parziale <eparziale@gmail.com>
2022-09-08 21:24:44 -0300

    :lipstick: update: change select2 to tom select

--- app/posts/static/js/posts.js
+++ app/posts/static/js/posts.js
@@ -1,7 +1,3 @@
-$( '#tags' ).select2( {
-    theme: "bootstrap-5",
-    width: $( this ).data( 'width' ) ? $( this ).data( 'width' ) : $( this ).hasClass( 'w-100' ) ? '100%' : 'style',
-    placeholder: $( this ).data( 'placeholder' ),
-    closeOnSelect: false,
-    allowClear: true
-} );
+new TomSelect("#tags",{
+    plugins: ['remove_button'],
+});
