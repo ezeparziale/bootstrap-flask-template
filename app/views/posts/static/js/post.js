3136af8b893dc1bf0930901c6315ece2e48c442e
Ezequiel Parziale <eparziale@gmail.com>
2022-10-11 21:51:27 -0300

    :lipstick: update: theme colors

--- app/views/posts/static/js/post.js
+++ app/views/posts/static/js/post.js
@@ -51,7 +51,7 @@ function disableComment(comment_id){
     .then((data) => {
         document.getElementById(`disable-comment-${comment_id}-txt`).innerHTML = data.text;
         if (data.disable) {
-            document.getElementById(`comment-${comment_id}`).innerHTML = `<span class="badge bg-warning">Comentario deshabilitado</span>`;
+            document.getElementById(`comment-${comment_id}`).innerHTML = `<span class="badge text-bg-warning">Comentario deshabilitado</span>`;
         } else {
             document.getElementById(`comment-${comment_id}`).innerHTML = data.content;
         }
