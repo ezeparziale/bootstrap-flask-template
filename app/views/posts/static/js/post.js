84f9ee478f67ad125eb2f529d652637ed0f67f06
Ezequiel Parziale <eparziale@gmail.com>
2023-01-30 21:18:14 -0300

    :zap: improve: delete post

--- app/views/posts/static/js/post.js
+++ app/views/posts/static/js/post.js
@@ -57,3 +57,12 @@ function disableComment(comment_id){
         }
     })
 }
+
+
+const modalDeletePost = document.getElementById('modalDeletePost')
+modalDeletePost.addEventListener('show.bs.modal', event => {
+  const button = event.relatedTarget
+  const url = button.getAttribute('data-bs-url')
+  const btn_delete = document.getElementById('btn-modal-delete')
+  btn_delete.href = url
+});
