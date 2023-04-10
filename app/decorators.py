4dee3fe5805821ab66772ebe5d28f0b463082040
Ezequiel Parziale <eparziale@gmail.com>
2023-04-10 20:13:46 -0300

    :rocket: update: add new decorator password_not_expired

--- app/decorators.py
+++ app/decorators.py
@@ -1,6 +1,6 @@
 from functools import wraps
 
-from flask import abort
+from flask import abort, flash, redirect, url_for
 from flask_login import current_user
 
 from .models import Permission
@@ -21,3 +21,16 @@ def permission_required(permission):
 
 def admin_required(f):
     return permission_required(Permission.ADMIN)(f)
+
+
+def password_not_expired(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not current_user.is_authenticated:
+            return abort(401)
+        if current_user.password_is_expired():
+            flash("Your password has expired. Please change it.", "danger")
+            return redirect(url_for("account.change_password"))
+        return f(*args, **kwargs)
+
+    return decorated_function
