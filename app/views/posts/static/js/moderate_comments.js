984730e07396db53c650fc81e3d4c95e74ac9eb6
Ezequiel Parziale <eparziale@gmail.com>
2023-04-15 23:52:03 -0300

    :lipstick: update: format style

--- app/views/posts/static/js/moderate_comments.js
+++ app/views/posts/static/js/moderate_comments.js
@@ -4,10 +4,6 @@ function disableComment(comment_id) {
         .then(resource => resource.json())
         .then((data) => {
             document.getElementById(`disable-comment-${comment_id}-txt`).innerHTML = data.text;
-            if (data.disable) {
-                document.getElementById(`comment-${comment_id}`).innerHTML = `<span class="badge text-bg-warning">Comentario deshabilitado</span>`;
-            } else {
-                document.getElementById(`comment-${comment_id}`).innerHTML = data.content;
-            }
+           
         })
 }
